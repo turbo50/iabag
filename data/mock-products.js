821ff96dd58f99e5df23e490a09b36bb52f72d60
NC6344 <aquilas-damico.lengue-lengue-@externe.grdf.fr>
2025-12-11 16:42:09 +0100

    Add mock

---
+++ data/mock-products.js
@@ -0,0 +1,184 @@
+export const MOCK_PRODUCTS = [
+  {
+    "marque_produit": "Luxe Paris",
+    "image_produit": "assets/images/products/P003-800-800.png",
+    "code_produit": "P006",
+    "prix_actuel": 249.99,
+    "ancien_prix": 320,
+    "nom_produit": "Sac à Main Prestige 2",
+    "notation": 4.9,
+    "liste_couleur": [
+      {
+        "name": "Gray",
+        "cssClass": "gray"
+      },
+      {
+        "name": "Orange",
+        "cssClass": "orange"
+      },
+      {
+        "name": "Red",
+        "cssClass": "red"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png"
+    ],
+    "sku": "LX-P003"
+  },
+  {
+    "marque_produit": "Luxe Paris",
+    "image_produit": "assets/images/products/P001-800-800.png",
+    "code_produit": "P001",
+    "prix_actuel": 199.99,
+    "ancien_prix": 249.99,
+    "nom_produit": "Sac Élégance Cuir Premium",
+    "notation": 4.7,
+    "liste_couleur": [
+      {
+        "name": "Green",
+        "cssClass": "green"
+      },
+      {
+        "name": "Orange",
+        "cssClass": "orange"
+      },
+      {
+        "name": "Red",
+        "cssClass": "red"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P001-800-960.png",
+      "assets/images/products/P001-800-960.png",
+      "assets/images/products/P001-800-960.png"
+    ],
+    "sku": "LX-P001"
+  },
+  {
+    "marque_produit": "Urban Chic",
+    "image_produit": "assets/images/products/P002-800-800.png",
+    "code_produit": "P002",
+    "prix_actuel": 99,
+    "ancien_prix": 129,
+    "nom_produit": "Sac à Main CityLife",
+    "notation": 4.3,
+    "liste_couleur": [
+      {
+        "name": "Yellow",
+        "cssClass": "yellow"
+      },
+      {
+        "name": "Pink",
+        "cssClass": "pink"
+      },
+      {
+        "name": "Red",
+        "cssClass": "red"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P002-800-960.png",
+      "assets/images/products/P002-800-960.png"
+    ],
+    "sku": "UC-P002"
+  },
+  {
+    "marque_produit": "Urban Chic",
+    "image_produit": "assets/images/products/P002-800-800.png",
+    "code_produit": "P005",
+    "prix_actuel": 99,
+    "ancien_prix": 129,
+    "nom_produit": "Sac à Main CityLife 2",
+    "notation": 4.3,
+    "liste_couleur": [
+      {
+        "name": "Green",
+        "cssClass": "green"
+      },
+      {
+        "name": "Orange",
+        "cssClass": "orange"
+      },
+      {
+        "name": "Red",
+        "cssClass": "red"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P002-800-960.png",
+      "assets/images/products/P002-800-960.png"
+    ],
+    "sku": "UC-P002"
+  },
+  {
+    "marque_produit": "Luxe Paris",
+    "image_produit": "assets/images/products/P001-800-800.png",
+    "code_produit": "P004",
+    "prix_actuel": 199.99,
+    "ancien_prix": 249.99,
+    "nom_produit": "Sac Élégance Cuir Premium 2",
+    "notation": 4.7,
+    "liste_couleur": [
+      {
+        "name": "Brown ",
+        "cssClass": "brown "
+      },
+      {
+        "name": "Orange",
+        "cssClass": "orange"
+      },
+      {
+        "name": "Navy blue",
+        "cssClass": "navy blue"
+      },
+      {
+        "name": "Aqua",
+        "cssClass": "aqua"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P001-800-960.png",
+      "assets/images/products/P001-800-960.png",
+      "assets/images/products/P001-800-960.png"
+    ],
+    "sku": "LX-P001"
+  },
+  {
+    "marque_produit": "Luxe Paris",
+    "image_produit": "assets/images/products/P003-800-800.png",
+    "code_produit": "P003",
+    "prix_actuel": 249.99,
+    "ancien_prix": 320,
+    "nom_produit": "Sac à Main Prestige",
+    "notation": 4.9,
+    "liste_couleur": [
+      {
+        "name": "Red",
+        "cssClass": "red"
+      },
+      {
+        "name": "Black",
+        "cssClass": "black"
+      },
+      {
+        "name": "Purple",
+        "cssClass": "purple"
+      },
+      {
+        "name": "Fuchsia",
+        "cssClass": "fuchsia"
+      }
+    ],
+    "autres_images": [
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png",
+      "assets/images/products/P003-800-960.png"
+    ],
+    "sku": "LX-P003"
+  }
+];
